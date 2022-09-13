6d19bdba9038bd0aeead7ae6c47426de9ac029de
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-09-13 17:55:04 +0900

    day7

--- My project/Assets/02.Script/05.Scene/ScaleManager.cs
+++ My project/Assets/02.Script/05.Scene/ScaleManager.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
- 
+
 
 public class ScaleManager : MonoBehaviour
 {
     [SerializeField] float MaxScale = 2f;
     [SerializeField] float MinScale = 0.5f;
     float rayDistance = 30f;
-    
+
     void Update()
     {
         ObjHit();
@@ -18,22 +18,21 @@ public class ScaleManager : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward * rayDistance);
         RaycastHit hitData;
 
-        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RHand))        
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")))
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")))
+            temp = hitData.collider.gameObject;
+            hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);
+        }
+        else
+        {
+            if (temp != null)
             {
-                temp = hitData.collider.gameObject;
-                hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);
+                temp.transform.localScale = new Vector3(MinScale, MinScale, MinScale);
+                temp = null;
             }
-            else
-            {
-                if (temp != null)
-                {
-                    temp.transform.localScale = new Vector3(MinScale, MinScale, MinScale);
-                    temp = null;
-                }
-            }
-        }     
+        }
+
     }
 }
 
