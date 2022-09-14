b74e7b9be032994e25d1b2ab8449d2189911e83d
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-09-14 18:19:14 +0900

    day8

--- My project/Assets/02.Script/05.Scene/ScaleManager.cs
+++ My project/Assets/02.Script/05.Scene/ScaleManager.cs
@@ -1,39 +1,67 @@
 using UnityEngine;
- 
 
 public class ScaleManager : MonoBehaviour
-{
-    [SerializeField] float MaxScale = 2f;
-    [SerializeField] float MinScale = 0.5f;
+{    
     float rayDistance = 30f;
-    
+    public GameObject CatWheel;
+    bool OnAnim = false;
+    public Animator anim;
+
+    public void Start()
+    {
+        CatWheel = GameObject.Find("CatWheelforUnity");
+
+        
+        if (anim != null)
+            Debug.Log("123");
+
+        else if (anim == null)
+            Debug.Log(anim);
+    }
+
+
     void Update()
     {
         ObjHit();
     }
-    GameObject temp;
+    
 
     public void ObjHit()
     {
-        Ray ray = new Ray(transform.position, transform.forward * rayDistance);
-        RaycastHit hitData;
-
-        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RHand))        
+        if (OVRInput.GetDown(OVRInput.RawButton.A))        
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")))
-            {
-                temp = hitData.collider.gameObject;
-                hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);
+            Ray ray = new Ray(transform.position, transform.forward * rayDistance);
+            RaycastHit hitData;
+            Debug.Log(anim.GetBool("OnAnimation"));
+         Debug.DrawRay(transform.position, transform.forward, Color.blue, rayDistance);
+
+            if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")) && !OnAnim)
+            {   
+                OnAnim = true;
+                anim.SetBool("OnAnimation", true);
+                Debug.Log("if");
+                //temp = hitData.collider.gameObject;
+                //hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);
+                
             }
+
+
             else
             {
+                OnAnim = false;
+                anim.SetBool("OnAnimation", false);
+            }
+
+            /*else
+            {
                 if (temp != null)
-                {
+               {
                     temp.transform.localScale = new Vector3(MinScale, MinScale, MinScale);
                     temp = null;
                 }
-            }
-        }     
+            }*/
+        }
     }
-}
 
+
+}
