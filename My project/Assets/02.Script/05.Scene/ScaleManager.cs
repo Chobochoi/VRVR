ab258cd32f94452d9c27d614cd894548822f8b7e
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-09-15 18:17:19 +0900

    kkkk

--- My project/Assets/02.Script/05.Scene/ScaleManager.cs
+++ My project/Assets/02.Script/05.Scene/ScaleManager.cs
@@ -1,67 +1,58 @@
 using UnityEngine;
 
+
 public class ScaleManager : MonoBehaviour
-{    
+{
     float rayDistance = 30f;
     public GameObject CatWheel;
     bool OnAnim = false;
     public Animator anim;
 
     public void Start()
-    {
-        CatWheel = GameObject.Find("CatWheelforUnity");
-
-        
-        if (anim != null)
-            Debug.Log("123");
-
-        else if (anim == null)
-            Debug.Log(anim);
+    {        
+        CatWheel = GameObject.Find("CatWheelforUnity");                
     }
-
 
     void Update()
     {
         ObjHit();
     }
-    
 
     public void ObjHit()
     {
-        if (OVRInput.GetDown(OVRInput.RawButton.A))        
-        {
+        
+        //if (OVRInput.GetDown(OVRInput.Button.Any))
+        //{
             Ray ray = new Ray(transform.position, transform.forward * rayDistance);
             RaycastHit hitData;
             Debug.Log(anim.GetBool("OnAnimation"));
-         Debug.DrawRay(transform.position, transform.forward, Color.blue, rayDistance);
+            //Debug.DrawRay(transform.position, transform.forward, Color.blue, rayDistance);
 
             if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")) && !OnAnim)
-            {   
+            {
                 OnAnim = true;
                 anim.SetBool("OnAnimation", true);
                 Debug.Log("if");
                 //temp = hitData.collider.gameObject;
-                //hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);
-                
+                //hitData.transform.localScale = new Vector3(MaxScale, MaxScale, MaxScale);                
             }
 
-
-            else
+            else if (Physics.Raycast(transform.position, transform.forward, out hitData, Mathf.Infinity, LayerMask.GetMask("Object")) && OnAnim)
             {
                 OnAnim = false;
                 anim.SetBool("OnAnimation", false);
             }
-
-            /*else
-            {
-                if (temp != null)
-               {
-                    temp.transform.localScale = new Vector3(MinScale, MinScale, MinScale);
-                    temp = null;
-                }
-            }*/
         }
-    }
+        
 
+        /*else
+        {
+            if (temp != null)
+           {
+                temp.transform.localScale = new Vector3(MinScale, MinScale, MinScale);
+                temp = null;
+            }
+        }*/
 
+    //}
 }
