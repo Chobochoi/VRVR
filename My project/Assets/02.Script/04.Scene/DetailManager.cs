b9550421dfefc4e6c05f0d51ef2a3caf617cf8fd
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-09-18 22:20:05 +0900

    day9

--- My project/Assets/02.Script/04.Scene/DetailManager.cs
+++ My project/Assets/02.Script/04.Scene/DetailManager.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class DetailManager : MonoBehaviour
-{   
+public class DetailManager : MonoBehaviourPunCallbacks
+{  
     public void DetailLoader()
     {
         switch(this.gameObject.name)
         {
-        case "Detail Button1":
+        case "Button1":
         SceneManager.LoadScene("01.Scenes/05.Detail1");
         break;
-        case "Detail Button2":
+        case "Button2":
         SceneManager.LoadScene("01.Scenes/05.Detail2");
         break;
-        case "Detail Button3":
+        case "Button3":
         SceneManager.LoadScene("01.Scenes/05.Detail3");
         break;
-        case "Detail Button4":
+        case "Button4":
         SceneManager.LoadScene("01.Scenes/05.Detail4");
         break;
+
+        DontDestroyOnLoad(gameObject);
         }
     }
+
+    
+
+    
 }
